fe0cf6783129493edd6561ef2ca59e32357b3373
Daniil Akmatov <akmatov.ceo@gmail.com>
2026-06-12 00:36:58 -0400

    HomeHeader screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,30 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import HomeHeader from "@/components/HomeHeader";
+import { globalStyles } from "@/styles/global";
+import { ScrollView, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>MacroZone</Text>
-      <Text style={styles.date}>Monday, March 16</Text>
-    </View>
+    <ScrollView style={globalStyles.container}>
+      <Text style={globalStyles.title}>MacroZone</Text>
+      <HomeHeader />
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#1a1a2e",
-    paddingTop: 60,
-    paddingHorizontal: 20,
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "#ffffff",
-  },
-  date: {
-    fontSize: 14,
-    color: "#a0a0b0",
-    marginTop: 4,
-    marginBottom: 30,
-  },
-});
